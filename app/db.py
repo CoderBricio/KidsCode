e849be4923ec833fc9922d8f9f151dd8fb13ccbf
SaraLopes7 <sara.r.a.lopes07@gmail.com>
2022-12-19 07:20:26 -0300

    ultimos retoques

--- app/db.py
+++ app/db.py
@@ -3,7 +3,7 @@ import mysql.connector
 #essas configurações deve ser mudada quando for configurar o servidor para deploy
 db = mysql.connector.connect( host="localhost",
                               user="root",
-                              password="MySQL@2022",
+                              password="root",
                               database="kidscode"
 )
 
