740c994ba1d9bff96f81da242561cbcacff6f952
CoderBricio <109881571+CoderBricio@users.noreply.github.com>
2022-12-22 10:55:37 -0300

    alinhamento final

--- app/db.py
+++ app/db.py
@@ -3,7 +3,7 @@ import mysql.connector
 #essas configurações deve ser mudada quando for configurar o servidor para deploy
 db = mysql.connector.connect( host="localhost",
                               user="root",
-                              password="root",
+                              password="Marecad2@",
                               database="kidscode"
 )
 
